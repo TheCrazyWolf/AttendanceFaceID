9c7ef5586bf6c6dc0100a83006bd09f033805f83
TheCraztWolf <tcrazywolf63@gmail.com>
2024-10-30 23:52:48 +0400

    Перечисление явки

---
+++ AttendanceFaceID.Services/Enums/AttendanceEnum.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttendanceFaceID.Services.Enums;
+
+public enum AttendanceEnum
+{
+    // дист
+    [Display(Name = "д/о")]
+    Distance,
+    // практика
+    [Display(Name = "п/п")]
+    Practice,
+    // явка
+    [Display(Name = "явка")]
+    Came,
+    // не явка
+    [Display(Name = "Н")]
+    Absent
+}
